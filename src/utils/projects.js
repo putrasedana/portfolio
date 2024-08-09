d52df0e396f583694e65b721c4ff1440ce0877e5
Putra Sedana <putrasedana03@gmail.com>
2024-08-10 00:03:44 +0800

    Menambahkan project Personal-Notes-v2

--- src/utils/projects.js
+++ src/utils/projects.js
@@ -2,37 +2,37 @@ const projects = [
   {
     image: "project1.png",
     title: "Nice Restaurant",
-    desc: "A restaurant website named 'Nice Restaurant' I created using HTML, CSS, and JavaScript. This website provides users with information about the restaurant.",
+    desc: "A restaurant website I created using HTML, CSS, and JavaScript. This website provides users with information about the restaurant.",
     images: ["html.png", "css.png", "javascript.png"],
     links: ["https://github.com/putrasedana/nice-restaurant", "https://nice-restaurant.netlify.app"],
   },
   {
     image: "project2.png",
     title: "Bookself App",
-    desc: "A Bookself App I created using HTML, CSS, and JavaScript. This web application allows users to input book data into shelves, move book data between shelves, delete book data from shelves, and edit books data.",
+    desc: "A Bookself App I created using HTML, CSS, and JavaScript. This web application allows users to input book data into shelves, move book between shelves, delete book from shelves, and edit books.",
     images: ["html.png", "css.png", "javascript.png"],
     links: ["https://github.com/putrasedana/bookshelf-app", "https://bookshelf-app01.netlify.app"],
   },
   {
     image: "project3.png",
     title: "Personal Notes",
-    desc: "The React-powered web application serves as a versatile tool for managing personal notes effectively. It fulfills the core requirements of a 'Personal Notes App' by enabling users to view, add, delete and edit notes seamlessly.",
+    desc: "The React-powered web application serves as a tool for managing personal notes effectively. It fulfills the core requirements of a 'Personal Notes App' by enabling users to view, add, delete and edit notes seamlessly.",
     images: ["react.png", "css.png"],
     links: ["https://github.com/putrasedana/personal-notes", "https://personal-notes01.netlify.app"],
   },
   {
     image: "project5.png",
-    title: "Food Delivery",
-    desc: "The React-powered web application, this project also using React Hook and React Router",
-    images: ["react.png", "css.png"],
-    links: ["https://github.com/putrasedana/food-delivery", "https://food-delivery0011.netlify.app"],
-  },
-  {
-    image: "project6.png",
     title: "Notes App",
     desc: "This is a native JavaScript project that utilizes Web Components, Webpack, and leverages a REST API for data handling.",
     images: ["html.png", "css.png", "javascript.png"],
     links: ["https://github.com/putrasedana/notes-app", "https://notes-app001.netlify.app"],
+  },
+  {
+    image: "project6.png",
+    title: "Personal Notes v2",
+    desc: "This React-powered web application is a Single Page Application (SPA) that leverages APIs, Context, and Hooks. It includes user authentication features, such as registration and login pages.",
+    images: ["react.png", "css.png"],
+    links: ["https://github.com/putrasedana/personal-notes-v2", "https://personal-notes02.netlify.app"],
   },
 ];
 
